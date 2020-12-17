e4fd2d82c8a613687f8e75112a04a939050b4427
oumaymabg <oumaymabou257@gmail.com>
2020-12-17 20:02:18 +0000

    amenity

--- models/amenity.py
+++ models/amenity.py
@@ -17,4 +17,4 @@ class Amenity(BaseModel, Base):
         place_amenities = relationship("Place", secondary=place_amenity,
                                        back_populates="amenities")
     else:
-        name = ""
+        name = "
