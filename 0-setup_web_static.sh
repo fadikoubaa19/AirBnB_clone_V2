afeecee8538347e9a2c6f6267a3272df97cedf8a
abdou-hidoussi <hidoussiabdou5@gmail.com>
2020-12-22 17:33:01 +0000

    initial

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# Task 0
+sudo apt-get install -y nginx
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+echo "All good and dandy" > /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+sudo sed -i '53i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo service nginx restart
