b7f30647a4d182aeb78b401952f9ebf147be54b9
abdou-hidoussi <hidoussiabdou5@gmail.com>
2020-12-22 17:58:22 +0000

    initial

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,14 +2,14 @@
 # Task 0
 sudo apt-get -y update
 sudo apt-get -y install nginx
-sudo mkdir -p /data/ /data/web_static/ /data/web_static/releases/ /data/web_static/shared/ /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/ /data/web_static/releases/test/
 sudo echo "<html>
   <head>
   </head>
   <body>
     Holberton School
   </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+</html>" | sudo tee /data/web_static/releases/test/index.html
 sudo rm -f -R /data/web_static/current
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
