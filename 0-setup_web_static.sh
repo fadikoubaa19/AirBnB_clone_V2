23f6551d13082f068cfbff27d0215533e2e019b5
abdou-hidoussi <hidoussiabdou5@gmail.com>
2020-12-22 17:44:49 +0000

    initial

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,7 +3,13 @@
 sudo apt-get install -y nginx
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
-echo "All good and dandy" | sudo tee /data/web_static/releases/test/index.html
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i '53i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
