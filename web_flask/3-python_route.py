f0ea258de576af84b730d4cb7db9c19225111e22
fadikoubaa19 <fadi.koubaa@gmail.com>
2021-01-20 18:38:52 +0100

    add the type /python/

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+''' task 2'''
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def home():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def main():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def route(text):
+    return "C {}".format(text.replace('_', ' '))
+
+
+@app.route("/python/")
+@app.route("/python/(<text>)", strict_slashes=False)
+def Python(text):
+    return "Python  {}".format(text.replace('_', ' '))
+
+
+if __name__ == "__main__":
+    app.run()
