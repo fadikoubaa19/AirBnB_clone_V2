2959362c13dff3aedde62c5d8117a29b6d2d4978
fadikoubaa19 <fadi.koubaa@gmail.com>
2021-01-20 18:23:34 +0100

    blankline

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 ''' task 0'''
 from flask import Flask
-
 app = Flask(__name__)
+
 
 @app.route("/", strict_slashes=False)
 def home():
     return "Hello HBNB!"
-    
+
+
 if __name__ == "__main__":
     app.run()
